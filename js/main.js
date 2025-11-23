86e568af739e4e0863ebd0bc63391e055a56c031
sinisa-terzic <sinisat.ita21@gmail.com>
2025-11-23 23:05:00 +0100

    upd animation pricing btn

--- js/main.js
+++ js/main.js
@@ -257,7 +257,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const button = document.getElementById(`showFullPrice-${sectionId}`);
         if (button) {
             button.classList.add('pulse');
-            setTimeout(() => button.classList.remove('pulse'), 200);
+            setTimeout(() => button.classList.remove('pulse'), 600);
         }
     }
 
